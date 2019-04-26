7fe4683359f81743770518b56176d502fd7d8346
Nemanja Micovic <nmicovic@outlook.com>
2019-04-26 12:06:55 +0000

    Azurirana build skripta i objavljeno Dusanovo predavanje

--- publish.sh
+++ publish.sh
@@ -1,7 +1,17 @@
-#! /bin/sh
+#! /usr/bin/env bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 echo 'Performing jekyll build...'
 jekyll build
+if [[ $? -ne 0  ]]; then
+    echo "${red}Failed Jekyll build.${reset}"
+    exit
+else
+    echo "${green}Successful Jekyll build!${reset}"
+fi
 
 echo 'Entering _site dir'
 cd _site
@@ -9,4 +19,9 @@ cd _site
 echo 'Publishing...'
 #read -p 'Enter your alas username: ' USER
 rsync -azP --exclude=.git --exclude=DONT_UPLOAD . risk@alas.matf.bg.ac.rs:/home/risk/public_html
-echo 'Published!'
+if [[ $? -ne 0  ]]; then
+    echo "${red}Failed publishing!${reset}"
+    exit
+else
+    echo "${green}Successfully published!${reset}"
+fi
